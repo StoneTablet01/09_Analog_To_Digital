5d0a1a4623e366a68d1c7504505e2930e029c7a2
Jim Sutton <jamespsutton3@cox.net>
2020-08-27 15:22:29 -0400

    working on ADC1 now instead of 2

--- main/adc2_example_main.c
+++ main/adc2_example_main.c
@@ -1,4 +1,4 @@
-/* ADC2 Example
+/* ADC1 Example
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -17,52 +17,43 @@
 #include "esp_system.h"
 
 #define DAC_EXAMPLE_CHANNEL     CONFIG_EXAMPLE_DAC_CHANNEL
-#define ADC2_EXAMPLE_CHANNEL    CONFIG_EXAMPLE_ADC2_CHANNEL
+#define ADC1_EXAMPLE_CHANNEL    CONFIG_EXAMPLE_ADC1_CHANNEL
 
-#if CONFIG_IDF_TARGET_ESP32
-static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
-#elif CONFIG_IDF_TARGET_ESP32S2
-static const adc_bits_width_t width = ADC_WIDTH_BIT_13;
-#endif
+// DAC on Pin 25 -- IO25
+// ADC on Pin 27 -- IO27
+
 
 void app_main(void)
 {
     uint8_t output_data=0;
-    int     read_raw;
+    int current_value=0;
     esp_err_t r;
 
     gpio_num_t adc_gpio_num, dac_gpio_num;
 
-    r = adc2_pad_get_io_num( ADC2_EXAMPLE_CHANNEL, &adc_gpio_num );
+    r = adc1_pad_get_io_num( ADC1_EXAMPLE_CHANNEL, &adc_gpio_num );
     assert( r == ESP_OK );
     r = dac_pad_get_io_num( DAC_EXAMPLE_CHANNEL, &dac_gpio_num );
     assert( r == ESP_OK );
 
-    printf("ADC2 channel %d @ GPIO %d, DAC channel %d @ GPIO %d.\n", ADC2_EXAMPLE_CHANNEL, adc_gpio_num,
+    printf("ADC1 channel %d @ GPIO %d, DAC channel %d @ GPIO %d.\n", ADC1_EXAMPLE_CHANNEL, adc_gpio_num,
                 DAC_EXAMPLE_CHANNEL + 1, dac_gpio_num );
 
     dac_output_enable( DAC_EXAMPLE_CHANNEL );
 
-    //be sure to do the init before using adc2. 
-    printf("adc2_init...\n");
-    adc2_config_channel_atten( ADC2_EXAMPLE_CHANNEL, ADC_ATTEN_0db );
+    //be sure to do the init before using ADC1.
+    printf("ADC1_init...\n");
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten( ADC1_EXAMPLE_CHANNEL, ADC_ATTEN_0db );
 
     vTaskDelay(2 * portTICK_PERIOD_MS);
 
     printf("start conversion.\n");
+
     while(1) {
         dac_output_voltage( DAC_EXAMPLE_CHANNEL, output_data++ );
-        r = adc2_get_raw( ADC2_EXAMPLE_CHANNEL, width, &read_raw);
-        if ( r == ESP_OK ) {
-            printf("%d: %d\n", output_data, read_raw );
-        } else if ( r == ESP_ERR_INVALID_STATE ) {
-            printf("%s: ADC2 not initialized yet.\n", esp_err_to_name(r));
-        } else if ( r == ESP_ERR_TIMEOUT ) {
-            //This can not happen in this example. But if WiFi is in use, such error code could be returned.
-            printf("%s: ADC2 is in use by Wi-Fi.\n", esp_err_to_name(r));
-        } else {
-            printf("%s\n", esp_err_to_name(r));
-        }
+        current_value = adc1_get_raw( ADC1_EXAMPLE_CHANNEL);
+        printf("%d\n", current_value );
 
         vTaskDelay( 2 * portTICK_PERIOD_MS );
     }
